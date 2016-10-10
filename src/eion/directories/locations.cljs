4fbbfb70279fe2008b8358c1063a0d23d99bc82f
Yury Solovyov <biohazard707@gmail.com>
2016-10-10 20:59:33 +0300

    dedupe

--- src/eion/directories/locations.cljs
+++ src/eion/directories/locations.cljs
@@ -5,33 +5,23 @@
             [eion.bindings.node :as node]
             [eion.bindings.electron :as electron]))
 
-(defn make-location
-  ([path name] (make-location path name nil))
-  ([path name mix]
-    (merge mix {:path path :name name})))
-
 (defn make-route [path]
-  { :route (npm/path-to-regexp (str path ":nested*")) })
+  (npm/path-to-regexp (str path ":nested*")))
+
+(defn make-location [path name]
+  {:path path :name name :route (make-route path) })
 
 (defn locations-posix []
-  (let [out-chan       (async/chan 1)
-        root-path      "/"
-        desktop-path   (electron/get-path "desktop")
-        documents-path (electron/get-path "documents")
-        downloads-path (electron/get-path "downloads")
-        music-path     (electron/get-path "music")
-        pictures-path  (electron/get-path "pictures")
-        videos-path    (electron/get-path "videos")
-        home-path      (electron/get-path "home")]
+  (let [out-chan (async/chan 1)]
     (async/put! out-chan [
-      (make-location root-path      "Root"      (make-route root-path))
-      (make-location desktop-path   "Desktop"   (make-route desktop-path))
-      (make-location documents-path "Documents" (make-route documents-path))
-      (make-location downloads-path "Downloads" (make-route downloads-path))
-      (make-location music-path     "Music"     (make-route music-path))
-      (make-location pictures-path  "Pictures"  (make-route pictures-path))
-      (make-location videos-path    "Videos"    (make-route videos-path))
-      (make-location home-path      "Home"      (make-route home-path))
+      (make-location "/" "Root")
+      (make-location (electron/get-path "desktop") "Desktop")
+      (make-location (electron/get-path "documents") "Documents")
+      (make-location (electron/get-path "downloads") "Downloads")
+      (make-location (electron/get-path "music") "Music")
+      (make-location (electron/get-path "pictures") "Pictures")
+      (make-location (electron/get-path "videos") "Videos")
+      (make-location (electron/get-path "home") "Home")
     ])
     out-chan))
 
